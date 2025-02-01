b1c4eb6dfb708ddd822117cffbc923a9b2d1297a
Ekramul <mdekramulhassan168@gmail.com>
2025-02-01 18:53:41 +0600

    add new code in contack components

--- src/components/contactus/contactUs.tsx
+++ src/components/contactus/contactUs.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Button from "../ui/button";
 import Image from "next/image";
+import { BiChevronRight } from "react-icons/bi";
 
 const ContactUs = () => {
   return (
-    <div className="relative bg-[url('https://res.cloudinary.com/dvtdneocc/image/upload/v1738250222/ctaMobileWaveLines_yt6sav.svg')] bg-cover bg-center bg-[#005BC4] text-white py-20 px-6 flex flex-col items-center text-center">
-      {/* Overlay for better text visibility */}
-      <div className="absolute inset-0  opacity-50"></div>
-
-      <div className="mx-auto">
-        {/* 3 Layered Images with Hover Effects */}
+    <figure className="z-10 [clip-path:polygon(0%_0%,100%_50%,100%_100%,0%_100%)] ">
+      <div className="relative h-[500px] bg-[url('https://res.cloudinary.com/dvtdneocc/image/upload/v1738250222/ctaMobileWaveLines_yt6sav.svg')] bg-cover bg-center bg-[#005BC4] text-white py-20 px-6 flex ">
         <div className="absolute inset-0 flex justify-center gap-4">
           <div className="relative w-1/3 hover:scale-105 transition-transform duration-500">
             <Image
@@ -20,39 +17,19 @@ const ContactUs = () => {
               className="rounded-lg transition-transform duration-500"
             />
           </div>
-          <div className="relative w-1/3 hover:scale-110 transition-transform duration-500">
-            <Image
-              src=""
-              alt="Layer 2"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg transition-transform duration-500"
-            />
-          </div>
-          <div className="relative w-1/3 hover:scale-115 transition-transform duration-500">
-            <Image
-              src=""
-              alt="Layer 3"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg transition-transform duration-500"
-            />
-          </div>
         </div>
-
-        {/* Content */}
-        <div className="relative z-10">
-          <h1 className="text-3xl font-bold">Legacy no longer</h1>
-          <p className="mt-4 max-w-lg">
+        <div className="mt-36 ml-20">
+          <h1 className="text-5xl font-bold ">Legacy no longer</h1>
+          <p className="mt-4 text-xl">
             Talk to us to find out how we can transform your organisation for
             the future.
           </p>
-          <Button className="mt-6 bg-[#FE8B53] hover:bg-[#fc7a40]">
-            Contact
+          <Button className="mt-6 bg-orange-500 flex justify-center items-center hover:bg-orange-400 transition-all duration-500">
+            Contact to Us <BiChevronRight />
           </Button>
         </div>
       </div>
-    </div>
+    </figure>
   );
 };
 
